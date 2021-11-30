32f703c2186a426f5f1ab026f39d316d426efdd9
Soliquidus <tibo.pfeifer@gmail.com>
2021-11-30 17:05:10 +0100

    Header and background generation for PDF added

--- pdf/certpdf.go
+++ pdf/certpdf.go
@@ -30,6 +30,13 @@ func (p *PdfSaver) Save(cert cert.Cert) error {
 	pdf.SetTitle(cert.LabelTitle, false)
 	pdf.AddPage()
 
+	// Background
+	background(pdf)
+
+	// --
+	// Header
+	header(pdf, &cert)
+
 	// save file
 	filename := fmt.Sprintf("%v.pdf", cert.LabelTitle)
 	path := path2.Join(p.OutputDir, filename)
@@ -39,4 +46,41 @@ func (p *PdfSaver) Save(cert cert.Cert) error {
 	}
 	fmt.Printf("Saved certificate to '%v'\n", path)
 	return nil
+}
+
+func header(pdf *gofpdf.Fpdf, c *cert.Cert) {
+	opts := gofpdf.ImageOptions{
+		ImageType: "png",
+	}
+
+	margin := 30.0
+	x := 0.0
+	imageWidth := 30.0
+	filename := "img/gopher.png"
+	pdf.ImageOptions(filename,
+		x+margin, 20,
+		imageWidth, 0,
+		false, opts, 0, "",
+	)
+
+	pageWidth, _ := pdf.GetPageSize()
+	x = pageWidth - imageWidth
+	pdf.ImageOptions(filename,
+		x-margin, 20,
+		imageWidth, 0,
+		false, opts, 0, "")
+
+	pdf.SetFont("Helvetica", "", 40)
+	pdf.WriteAligned(0, 50, c.LabelCompletion, "C")
+}
+
+func background(pdf *gofpdf.Fpdf) {
+	opts := gofpdf.ImageOptions{
+		ImageType: "png",
+	}
+	pageWidth, pageHeight := pdf.GetPageSize()
+	pdf.ImageOptions("img/background.png",
+		0, 0,
+		pageWidth, pageHeight,
+		false, opts, 0, "")
 }
